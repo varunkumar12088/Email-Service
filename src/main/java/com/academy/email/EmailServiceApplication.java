274ea5a788fee528d43055471a315a89a4ce6f99
Varun Kumar <varun.kumar12aug@gmail.com>
2025-07-06 21:36:16 +0530

    Updated based on the error

--- src/main/java/com/academy/email/EmailServiceApplication.java
+++ src/main/java/com/academy/email/EmailServiceApplication.java
@@ -2,8 +2,12 @@ package com.academy.email;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.academy"})
+@EnableMongoRepositories(basePackages = {"com.academy"})
 public class EmailServiceApplication {
 
 	public static void main(String[] args) {
